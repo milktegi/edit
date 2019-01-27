a34c1e3cb33e57fa43b92b3139ee2af5db86b2cb
milktegi <milkteegi@gmail.com>
2019-01-27 18:20:36 +0900

    added redux

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+	return(
+		<div>
+			editorApp
+		</div>
+	)
+}
+
+export default App;
---
+++ src/store/configure.js
@@ -0,0 +1,30 @@
+import { 
+	createStore, 
+  applyMiddleware, 
+	compose, 
+	combineReducers } 
+from 'redux';
+
+import penderMiddleware from 'redux-pender';
+import * as modules from './modules';
+
+
+const reducers = combineReducers(modules);
+const middlewares = [penderMiddleware()];
+
+// 개발 모드일때 
+// redux devtool
+
+const isDev = process.env.NODE_ENV === 'development';
+const devtools = isDev && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__;
+const composeEnhancers = devtools || compose;
+
+// preloadedState는 추후 서버사이드 렌더링
+// 했을 떼 전달받는 초기 상태임 
+
+const configure = (preloadedState) => 
+createStore(reducers, 
+            preloadedState, 
+						composeEnhancers(applyMiddleware(...middlewares)));
+
+export default configure;
