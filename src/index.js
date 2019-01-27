b5d556ca2d16d52e6ae5ad1784abe482692ceb94
milktegi <milkteegi@gmail.com>
2019-01-27 18:44:33 +0900

    added global style scss

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Root from './Root';
+import './styles/base.scss';
+
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<Root />, document.getElementById('root'));
