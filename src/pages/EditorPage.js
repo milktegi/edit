62c5ef48def29414e680397029e24824c42ae35e
milktegi <milkteegi@gmail.com>
2019-01-27 22:12:24 +0900

    added editor layout

--- src/pages/EditorPage.js
+++ src/pages/EditorPage.js
@@ -1,9 +1,13 @@
 import React from 'react';
-
+import EditorTemplate from 'components/editor/EditorTemplate';
 const EditorPage = () => {
 	return (  
 		<div>
-			EditorPage 
+			<EditorTemplate
+			header="헤더"
+			editor="에디터"
+			preview="프리뷰"
+			/>
 		</div>
 	);
 }
